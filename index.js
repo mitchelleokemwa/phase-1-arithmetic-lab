b740337b05b84665089923fdeb879ba00e28d0c1
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-12-03 23:24:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4 ;
+
+const max = Math.max(1,6,12,14,20);
